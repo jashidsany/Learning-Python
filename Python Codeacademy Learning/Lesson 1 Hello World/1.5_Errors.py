fa6caaa733379b4c95f169401afc1cdef8020c70
Jashid_Sany <jashid.sany@gmail.com>
2020-12-14 16:46:36 -0500

    Delete 1.5_Errors.py

--- Python Codeacademy Learning/Lesson 1 Hello World/1.5_Errors.py
+++
@@ -1,4 +0,0 @@
-print("This message has mismatched quote marks!")
-print('Abracadabra')
-# error in the first ended string with mismatched quotation marks
-# error in the second was that string did not have any quotation marks
