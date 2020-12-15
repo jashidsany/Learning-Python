502704350aa54187dbe98c13c0eeffec409d70ec
Jashid_Sany <jashid.sany@gmail.com>
2020-12-14 20:25:22 -0500

    Add files via upload

---
+++ Codeacademy Lesson 2 Functions/L1.3_Parameters.py
@@ -0,0 +1,16 @@
+# def mult_two_add_three():
+#   number = 5
+#   print(number*2 + 3)
+
+def mult_two_add_three(number):
+  print(number*2 + 3)
+
+
+# Call mult_two_add_three() here:
+mult_two_add_three(1)
+print("\n")
+mult_two_add_three(5)
+print("\n")
+mult_two_add_three(-1)
+print("\n")
+mult_two_add_three(0)
