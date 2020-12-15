a42dbdbbea2818037d7602244a1201e8cc232ceb
Jashid_Sany <jashid.sany@gmail.com>
2020-12-15 11:56:08 -0500

    Delete L1.2_Whitespace.py

--- Codeacademy Lesson 2 Functions/L1.2_Whitespace.py
+++
@@ -1,6 +0,0 @@
-# The level of indenation tells the computer whats part of the function and what isn't.
-def about_this_computer():
-  print("This computer is running on version Everest Puma")
-
-print("This is your desktop")
-about_this_computer()
