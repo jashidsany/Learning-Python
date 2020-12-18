d66bb0f527dbe066be4b2c042860a1435f9da66e
Jashid_Sany <jashid.sany@gmail.com>
2020-12-18 15:05:28 -0500

    Add files via upload

---
+++ Codecademy Lesson 4 Lists/LA1.11_Selecting_List_Elements_1.py
@@ -0,0 +1,7 @@
+# Index can be called by name[]
+employees = ['Michael', 'Dwight', 'Jim', 'Pam', 'Ryan', 'Andy', 'Robert']
+index4 = (employees[4])
+print(len(employees))
+
+# Outputs an error
+print(employees[8]) 
