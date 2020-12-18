ef99af7fe496b0712060be2dac309be0918bb4cd
Jashid_Sany <jashid.sany@gmail.com>
2020-12-18 15:06:07 -0500

    Add files via upload

---
+++ Codecademy Lesson 4 Lists/LA1.16_Sorting_Lists_1.py
@@ -0,0 +1,18 @@
+### Exercise 1 & 2 ###
+addresses = ['221 B Baker St.', '42 Wallaby Way', '12 Grimmauld Place', '742 Evergreen Terrace', '1600 Pennsylvania Ave', '10 Downing St.']
+addresses.sort()
+print(addresses)
+
+# Sort addresses here:
+
+### Exercise 3 ###
+names = ['Ron', 'Hermione', 'Harry', 'Albus', 'Sirius']
+names.sort()
+print(names)
+
+### Exercise 4 ###
+cities = ['London', 'Paris', 'Rome', 'Los Angeles', 'New York']
+
+sorted_cities = cities.sort() # you can't store it in a variable
+print(sorted_cities)
+print(cities)
