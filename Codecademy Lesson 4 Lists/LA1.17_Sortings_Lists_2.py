d753c63f9acdbbb7ee446ea9b3a90c7bb0bd1877
Jashid_Sany <jashid.sany@gmail.com>
2020-12-18 15:06:17 -0500

    Add files via upload

---
+++ Codecademy Lesson 4 Lists/LA1.17_Sortings_Lists_2.py
@@ -0,0 +1,8 @@
+# A second way of sorting a list is to use sorted. sorted is different from .sort() in several ways:
+# It comes before a list, instead of after.
+# It generates a new list
+
+games = ['Portal', 'Minecraft', 'Pacman', 'Tetris', 'The Sims', 'Pokemon']
+games_sorted = sorted(games)
+print(games)
+print(games_sorted)
