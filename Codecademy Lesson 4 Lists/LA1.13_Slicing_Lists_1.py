377c12252b5fc33a3a59c44e3a49bac69f976bd3
Jashid_Sany <jashid.sany@gmail.com>
2020-12-18 15:05:44 -0500

    Add files via upload

---
+++ Codecademy Lesson 4 Lists/LA1.13_Slicing_Lists_1.py
@@ -0,0 +1,10 @@
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# Suppose we want to select from b through f.
+# We can do this using the following syntax: letters[start:end], where:
+# The last element we want is f, which has index 5, so end needs to be 6
+
+suitcase = ['shirt', 'shirt', 'pants', 'pants', 'pajamas', 'books']
+beginning = suitcase[0:4]
+print(beginning)
+
+middle = suitcase[2:4]
