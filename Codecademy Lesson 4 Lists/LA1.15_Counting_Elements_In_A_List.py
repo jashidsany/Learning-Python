b3596963ad1f7b94b39534677e288c1984f223c9
Jashid_Sany <jashid.sany@gmail.com>
2020-12-18 15:05:59 -0500

    Add files via upload

---
+++ Codecademy Lesson 4 Lists/LA1.15_Counting_Elements_In_A_List.py
@@ -0,0 +1,6 @@
+# If we want to know how many times 'Jake' appears in this group, we can use the function count:
+
+votes = ['Jake', 'Jake', 'Laurie', 'Laurie', 'Laurie', 'Jake', 'Jake', 'Jake', 'Laurie', 'Cassie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie']
+
+jake_votes = votes.count('Jake')
+print(jake_votes)
