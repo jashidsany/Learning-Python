591c55eae73cedf1bea93698ee86eb848be848a7
Jashid_Sany <jashid.sany@gmail.com>
2021-01-03 21:02:59 -0500

    Delete L10.1_Class.py

--- Codecademy Lesson 10 Classes/L10.1_Class.py
+++
@@ -1,7 +0,0 @@
-# A class is a template for a data type.
-# It describes the kinds of information that class will hold and how a programmer will interact with that data.
-# Define a class using the class keyword.
-# PEP 8 Style Guide for Python Code recommends capitalizing the names of classes to make them easier to identify.
-
-class Facade:
-  pass
