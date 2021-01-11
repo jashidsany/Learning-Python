8b49569e90fbf51869ff8e855d5135eb120dcbdd
Jashid_Sany <jashid.sany@gmail.com>
2021-01-11 17:57:15 -0500

    Update LA11.13_Review.py

--- LCodecademy Lesson 11 Function Arguments/LA11.13_Review.py
+++ LCodecademy Lesson 11 Function Arguments/LA11.13_Review.py
@@ -9,4 +9,4 @@
 # How to pass a list as a series of arguments to a function
 # How to pass a dictionary as a series of keyword arguments to a function
 # This is a lot, and you should be impressed with yourself! You now should be able to read many different styles of function writing in Python and come up with ways to call those functions with style and clarity.
-# Hopefully this has helped you as a writer of Python functions and enabled you to overcome any problems with input and output of a Python function you might run into. Congrats!
+# Hopefully this has helped you as a writer of Python functions and enabled you to overcome any problems with input and output of a Python function you might run into. Congrats
