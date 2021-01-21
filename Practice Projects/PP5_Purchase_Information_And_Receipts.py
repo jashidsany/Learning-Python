468e32051752a8e6c23651248f23dd5a75762cdd
Jashid_Sany <jashid.sany@gmail.com>
2021-01-20 19:28:51 -0500

    Update PP5_Purchase_Information_And_Receipts.py

--- Practice Projects/PP5_Purchase_Information_And_Receipts.py
+++ Practice Projects/PP5_Purchase_Information_And_Receipts.py
@@ -1,4 +1,4 @@
-# Uses concatanation to combine strings
+# Uses concatanation to combine strings.
 lovely_loveseat_description = """Lovely Loveseat: Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white. """
 lovely_loveseat_price = 254.00
 
